41fc8e466d837f3de7b6946a40b70d6b09b4ef98
Keith Tan <skeithtan@gmail.com>
2018-09-01 07:42:00 +0800

    Add ClassSchedule to entity

---
+++ src/entity/class_schedule.ts
@@ -0,0 +1,39 @@
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToMany, ManyToOne } from "typeorm";
+import { IsNotEmpty } from "class-validator";
+import { FacultyMember, Subject } from "./";
+import { MeetingDays, MeetingHours } from "../enum";
+
+@Entity()
+export default class ClassSchedule extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("enum", { enum: MeetingDays })
+    meetingDays: MeetingDays;
+
+    @Column("enum", { enum: MeetingHours })
+    meetingHours: MeetingHours;
+
+    @Column()
+    @IsNotEmpty()
+    room: string;
+
+    @Column()
+    enrollmentCap: number;
+
+    @Column()
+    course: string;
+
+    @Column()
+    section: string;
+
+    //
+    // ─── Relations ───────────────────────────────────────────────────────────────────────────
+    //
+
+    @ManyToOne((type?: any) => FacultyMember, (fm: FacultyMember) => fm.assignedClassSchedules)
+    assignedFacultyMember: FacultyMember;
+
+    @ManyToOne((type?: any) => Subject, (s: Subject) => s.classSchedules)
+    subject: Subject;
+}
