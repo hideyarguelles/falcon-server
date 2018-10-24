ebaa91ff630a1104f0068f8bd40d7c63435dce7b
Keith Tan <skeithtan@gmail.com>
2018-10-24 09:30:50 +0800

    Fix delete status

--- src/views/class_schedule.ts
+++ src/views/class_schedule.ts
@@ -1,3 +1,4 @@
+import * as status from "http-status-codes";
 import { Context } from "koa";
 import ClassScheduleController from "../controllers/class_schedule";
 import { UserType } from "../enums";
@@ -8,6 +9,8 @@ export default class ClassScheduleView extends View<ClassScheduleController> {
     @RequireAuthorization([UserType.Clerk, UserType.Dean, UserType.AssociateDean])
     remove = async (ctx: Context): Promise<void> => {
         const { classScheduleId } = ctx.params;
-        await this.controller.remove(classScheduleId);
+        await this.controller.remove(classScheduleId).then(() => {
+            ctx.status = status.NO_CONTENT;
+        });
     };
 }
