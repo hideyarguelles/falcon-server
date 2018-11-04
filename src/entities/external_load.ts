0ea588e7e167ceb1a57135a2c272fa7e5d46503e
hideyarguelles <catherine_arguelles@yahoo.com>
2018-11-04 12:39:09 +0800

    Add primary generated column

--- src/entities/external_load.ts
+++ src/entities/external_load.ts
@@ -1,13 +1,16 @@
-import { BaseEntity, Entity, ManyToOne } from "typeorm";
+import { BaseEntity, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import FacultyMember from "./faculty_member";
 import Term from "./term";
 
 @Entity()
 export default class ExternalLoad extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
     //
     // ─── Relations ───────────────────────────────────────────────────────────────────────────
     //
-    @ManyToOne((type?: any) => FacultyMember, (fm: FacultyMember) => fm.timeConstraints)
+    @ManyToOne((type?: any) => FacultyMember, (fm: FacultyMember) => fm.externalLoads)
     facultyMember: FacultyMember;
 
     @ManyToOne((type?: any) => Term, (t: Term) => t.externalLoads)
